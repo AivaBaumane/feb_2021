111a2edb351393cb1ed364f7b69f1141d15aba0c
Aiva Baumane <aiva.baumane@gmail.com>
2021-02-11 20:59:15 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "february_11_2021"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
---
+++ src/main/scala/uzdevums2.scala
@@ -0,0 +1,30 @@
+object uzdevums2 extends App {
+  def getCityYear(p0:Float, perc:Float, delta:Int, p1:Int):Unit = {
+    println("Greetings!")
+    val years=p0*(perc/100)+delta.toFloat
+    var i=p0
+    do {
+      i+=years
+    } while (i<p1)
+    val xvalue=(i-p0)/years
+    println(s"Population  reaches $p1 after $xvalue years")
+  }
+
+  getCityYear(1000,2,50,1200)
+
+}
+
+////  def getCityYear
+//  //Write a function which takes 4 parameters p0, perc, delta, p
+//  //return integer of years when the population reaches p
+//  //yearly formula is previous year * percentage increas + delta
+//  //percentage is in percent so you will need to convert
+//  //delta is how many people leave or join the city every year
+//  //probably while loop will work best here
+//  //also consider that it is possible that city never reaches p, then you should return -1
+//  //hint: if population does not increase after first year it will never reach p
+//
+////  println(getCityYear(1000,2,50,1200)) should print 3
+////  println(getCityYear(1000,2,-50,1200)) should print -1
+////  println(getCityYear(1500000,2.5,10000,2000000)) should print 10
+//}
